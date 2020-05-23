c4908b34ff969a3863dc26a36743e69fc012d830
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-23 13:36:39 +0300

    functions to save,delete and update location

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,14 @@ class Location(models.Model):
     place=models.CharField(max_length=50)
     def __str__(self):
         return self.place
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.delete()
+    @classmethod
+    def update(cls,id,name):
+        location = Location.objects.filter(id=id)
+        location.update(place=name)
 
 
 class Category(models.Model):
