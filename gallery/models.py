90c3f53b6d2b61392517043678c4efb3a75c99eb
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-23 12:41:01 +0300

    add Category model

--- gallery/models.py
+++ gallery/models.py
@@ -5,3 +5,10 @@ class Location(models.Model):
     place=models.CharField(max_length=50)
     def __str__(self):
         return self.place
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
